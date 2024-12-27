c3c9f57b481523331491217d445dd045ae21265a
jkk290 <158115736+jkk290@users.noreply.github.com>
2024-12-26 21:22:40 -1000

    Add psuedocode to scripts.js file
    Created psuedocode to javascript file to help
    break down the solution to this project.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello there")
+// Randomly determine which choice the computer will have
+// Get the choice from the player
+// Store and track the players score
+// Store and track the computers score
+// store and track rounds
+// Compare the players choice to computers choice
+// If player choice wins, increment player score
+// If computer choice wins, increment computer score
+// If player choice equals computer choice, re-select computer choice and get new choice from player
+// Repeat comparing steps until winner
+// Increase round by 1 after each win
+// Replay game until 5 rounds complete
+// Announce overall winner after 5 rounds
