aa7123783b6441da3368f48d562d295ec1ac945f
jkk290 <158115736+jkk290@users.noreply.github.com>
2024-12-27 19:19:41 -1000

    Add the playRound function
    Started creating the playRound function for the
    rock paper scissors game.

--- scripts.js
+++ scripts.js
@@ -9,7 +9,6 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
-console.log(getComputerChoice())
 
 // Get the choice from the player
 function getHumanChoice() {
@@ -18,11 +17,48 @@ function getHumanChoice() {
 }
 console.log(getHumanChoice());
 // Store and track the players score
+let humanScore = 0;
+
 // Store and track the computers score
-// store and track rounds
+let computerScore = 0;
+
+// Store and track rounds
 // Compare the players choice to computers choice
 // If player choice wins, increment player score
 // If computer choice wins, increment computer score
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lost, you had chose rock and the computer chose paper.");
+        computerScore++;
+        console.log("Current score: You (" + humanScore + ") - Computer (" + computerScore + ")" );
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lost, you had chose paper and the computer chose scissors.");
+        computerScore++;
+        console.log("Current score: You (" + humanScore + ") - Computer (" + computerScore + ")" );
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lost, you had chose scissors and the computer chose rock.");
+        computerScore++;
+        console.log("Current score: You (" + humanScore + ") - Computer (" + computerScore + ")" );
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You won, you had chose rock and the computer chose scissors.");
+        humanScore++;
+        console.log("Current score: You (" + humanScore + ") - Computer (" + computerScore + ")" );
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You won, you had chose paper and the computer chose rock.");
+        humanScore++;
+        console.log("Current score: You (" + humanScore + ") - Computer (" + computerScore + ")" );
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You won, you had chose scissors and the computer chose paper.");
+        humanScore++;
+        console.log("Current score: You (" + humanScore + ") - Computer (" + computerScore + ")" );
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection.toLowerCase(),computerSelection.toLowerCase());
+
 // If player choice equals computer choice, re-select computer choice and get new choice from player
 // Repeat comparing steps until winner
 // Increase round by 1 after each win
